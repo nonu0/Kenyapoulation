a2246ec342da5ff1872ff0cdfd397aa9648c4935
cliff <cliffowino22@gmail.com>
2023-03-14 10:45:23 +0300

    staticfiles

--- core/settings.py
+++ core/settings.py
@@ -111,22 +111,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = 'static/'
-# s3
-AWS_ACCESS_KEY_ID = 'AKIATU45KNXNY2KYO7H4'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'kenyapopulation-bucket'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mysite/static'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# s3
+# AWS_ACCESS_KEY_ID = 'AKIATU45KNXNY2KYO7H4'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'kenyapopulation-bucket'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',
+# }
+# AWS_LOCATION = 'static'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'mysite/static'),
+# ]
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # Default primary key field type
